c59428bd2530ef3adc1cb88182635a0078ff3421
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-04-29 19:51:38 -0400

    changed logic of adding custom properties; but need further investigation why the github and school are undefined

--- src/page-template.js
+++ src/page-template.js
@@ -6,6 +6,8 @@ const generateHTML = employeeInfo => {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="style.css" />
     <title>Company Employees</title>
     </head>
 <body>
@@ -25,8 +27,7 @@ const generateHTML = employeeInfo => {
 
 const appendEmployee = function(employeeInfo){
 var employees = ``;
-    if(employeeInfo.length>1){
-        for(let i=1; i<employeeInfo.length; i++){
+        for(let i=0; i<employeeInfo.length; i++){
             employees=employees+`        
                 <div class="card">
                     <h3 class="card-header">${employeeInfo[i].name}</h3>
@@ -34,15 +35,31 @@ var employees = ``;
                     <p class="role">${employeeInfo[i].role}</p> 
                     <p class="id">${employeeInfo[i].id}</p>
                     <p class="email">${employeeInfo[i].email}</p>
-                    <p class="custom">${employeeInfo[i].github || employeeInfo[i].officeNumber || employeeInfo[i].school} </p>
+                    <p class="custom">${appendCustom(employeeInfo[i])}</p>
                 </div>`
                 
         }
         return employees;
-    } else {
-        return;
-    }
 }
+
+const appendCustom = function(employeeInfo) {
+var custom = ``;
+            //if employee is an engineer
+            if(employeeInfo.github){
+                custom=custom+ 'Github Username: ' + employeeInfo.github;
+            } 
+            //if employee is a manager
+            else if(employeeInfo.officeNumber){
+                custom=custom+ 'Office Number: '+employeeInfo.officeNumber;
+            } 
+            //if the employee is a intern
+            else {
+                custom=custom+ 'University: '+ employeeInfo.school;
+            }
+        return custom;
+}
+
+
 
 //do not use any () when exporting as it will invoke it automatically.
 module.exports = generateHTML;
